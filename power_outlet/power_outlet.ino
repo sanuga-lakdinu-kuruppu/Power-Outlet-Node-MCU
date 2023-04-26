58efbf342abd3c8ffb90bb01ad49421618422432
Sanuga Lakdinu Kuruppu <cohdse221f-015@student.nibm.lk>
2023-04-26 11:42:08 +0530

    read sensor and device status is okay

--- power_outlet/power_outlet.ino
+++ power_outlet/power_outlet.ino
@@ -1,125 +1,93 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
+#include "DHT.h"
 
-#define WIFI_SSID "HUAWEI Y9 2019" //Device wifi username
-#define WIFI_PASSWORD "SSss11.." // Device wifi password
 
-#define FIREBASE_HOST "test-58844-default-rtdb.asia-southeast1.firebasedatabase.app" // Firebase host
-#define FIREBASE_AUTH "Y2lmJfXlEvdxX7fwF0O64arhWNI1RRc46b0wPlij" //Firebase Auth code
+#define WIFI_SSID "Test"          //Device wifi username
+#define WIFI_PASSWORD "12345678"  // Device wifi password
 
-#define pinDHT11INPUT D1
-#define pinACOUTPUT D0
-#define typeDHT11 DHT11
+#define FIREBASE_HOST "test-58844-default-rtdb.asia-southeast1.firebasedatabase.app"  // Firebase host
+#define FIREBASE_AUTH "Y2lmJfXlEvdxX7fwF0O64arhWNI1RRc46b0wPlij"                      //Firebase Auth code
 
-#define pinWIFISTATUSOUTPUT D6
-#define pinFIREBASESTATUSOUTPUT D7
-#define pinPOWEROUTPUTSTATUSOUTPUT D8
-#define pinACOUTPUT D0
-
-unsigned long timePrevMillis = 0;
-bool deviceStatus = false;
+#define OUTPUT_STATUS_PIN D0
 
 //Temperature Sensor Configuration
-DHT dhtObj(pinDHT11INPUT,typeDHT11);
+#define TEMPERATURE_TYPE DHT11
+#define TEMPERATURE_PIN D1
+
+DHT dhtObj(TEMPERATURE_PIN, TEMPERATURE_TYPE);
+
+bool OUTPUT_STATUS = false;
+unsigned long TIMEPREVIOURMILLS_TEMP = 0;
 
 void setup() {
- 
- Serial.begin(9600);
+
+  Serial.begin(9600);
 
   //Pin Configuration
-  pinMode(pinDHT11INPUT,INPUT);
-  pinMode(pinACOUTPUT,OUTPUT);
-  pinMode(pinWIFISTATUSOUTPUT,OUTPUT);
-  pinMode(pinFIREBASESTATUSOUTPUT,OUTPUT);
-  pinMode(pinPOWEROUTPUTSTATUSOUTPUT,OUTPUT);
+  pinMode(OUTPUT_STATUS_PIN, OUTPUT);
+  pinMode(TEMPERATURE_PIN, INPUT);
 
-  //Wifi Configuration  
+  //Wifi Configuration
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print(F("Connecting"));
   while (WiFi.status() != WL_CONNECTED) {
-    digitalWrite(pinWIFISTATUSOUTPUT,HIGH);
     Serial.print(F("."));
     delay(300);
   }
-  digitalWrite(pinWIFISTATUSOUTPUT,LOW);
   Serial.println();
   Serial.println(F("Connected."));
   Serial.println(WiFi.localIP());
-  Serial.println();  
+  Serial.println();
 
   //Firebase Configuration
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Serial.print(F("Firebase Connecting.."));
 
-  while(Firebase.failed() == true){
-    digitalWrite(pinFIREBASESTATUSOUTPUT,HIGH);
+  while (Firebase.failed() == true) {
     Serial.print(F("."));
-    delay(300);    
-    
+    delay(300);
   }
-  digitalWrite(pinFIREBASESTATUSOUTPUT,LOW);
   Serial.println();
   Serial.println(F("Firebase Connected."));
-  Serial.println();  
-      
-  Firebase.setString("DEVICES/EIFHa45JS/DEVICE_STATUS","Online");
-  
+  Serial.println();
+
+  //initializing dht sensor
   dhtObj.begin();
 }
 
 void loop() {
-  
-  //Pass Sensor Data to Database
-  if( (millis() - timePrevMillis > 2000 || timePrevMillis == 0)){
-        timePrevMillis = millis();
-        
-        //read temperature sensor data
-        float temp = dhtObj.readTemperature();
-        float hum = dhtObj.readHumidity();
 
-        //check read data is correct
-        if(isnan(temp) || isnan(hum)){
-          Serial.println(F("Failed to read from DHT sensor!"));          
-          return;
-        }
+  //getting output_status every time
+  OUTPUT_STATUS = Firebase.getBool("HOUSES/HOUSES_1/ADAPTORS/ADAPTOR_1/OUTPUT_STATUS");
 
-          Firebase.setFloat("DEVICES/EIFHa45JS/TEMPERATURE",temp);
-          Firebase.setFloat("DEVICES/EIFHa45JS/HUMIDITY",hum);
-        
-          Serial.print(F("Temperature Sensor data is stored.."));
-          Serial.print(F("Temp : "));  
-          Serial.print(temp);
-          Serial.print(F("Hum : "));  
-          Serial.println(hum);
-          Serial.print(F("Path : "));
-          Serial.println(F("DEVICES/EIFHa45JS/\n"));                    
-   }
+  //setting output
+  if (OUTPUT_STATUS) {
+    digitalWrite(OUTPUT_STATUS_PIN, HIGH);
+  } else {
+    digitalWrite(OUTPUT_STATUS_PIN, LOW);
+  }
 
-}
-
-bool checkDeviceStatus(){
-  if(Firebase.getString("DEVICES/EIFHa45JS/DEVICE_STATUS") == "Online"){
-    return true;
-  }  
-  else{
-    return false;
+  //every 5 second read temperature data and set inside the database
+  if (millis() - TIMEPREVIOURMILLS_TEMP > 5000 || TIMEPREVIOURMILLS_TEMP == 0) {
+    TIMEPREVIOURMILLS_TEMP = millis();
+    readTemperatureData();
   }
 }
 
-bool checkDeviceOkay(){
-  if(Firebase.getBool("DEVICES/EIFHa45JS/IS_OKAY") == true){
-    return true;
-  }  
-  else{
-    return false;
-  }  
-}
+void readTemperatureData() {
 
-bool checkDevicePowerOutputStatus(){
-  if(Firebase.getBool("DEVICES/EIFHa45JS/POWER_OUTPUT_STATUS") == true){
-    return true;
-  }  
-  else{
-    return false;
-  }   
+  //read temperature sensor data
+  float temp = dhtObj.readTemperature();
+  float hum = dhtObj.readHumidity();
+
+  //check read data is correct
+  if (isnan(temp) || isnan(hum)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  //set data inside the database
+  Firebase.setFloat("HOUSES/HOUSES_1/ADAPTORS/ADAPTOR_1/TEMPERATURE/CURRENT", temp);
+  Firebase.setFloat("HOUSES/HOUSES_1/ADAPTORS/ADAPTOR_1/HUMIDITY/CURRENT", hum);
 }
